2a12dc39f2c953884b9397b14d7d45a0950b96d7
Abdullah Safwan <abdullah.safwan@vodafone.co.uk>
2023-10-26 12:34:24 +0100

    Cart.js Completed

---
+++ classes/Cart.js
@@ -0,0 +1,20 @@
+let Product = require("./Product");
+
+class Cart {
+  constructor(products, total) {
+    this.products = [];
+    this.total = 0;
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+    this.total += product["price"];
+  }
+
+  removeProduct(product) {
+    this.total -= this.products[product].price;
+    this.products.splice(product, 1);
+  }
+}
+
+module.exports = Cart;
